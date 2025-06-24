b2964930ca8608602b0e93ee6b26fb304673d55d
Juncheng Ma <101777868+ucasmjc@users.noreply.github.com>
2025-06-24 19:58:47 +0800

    Update download_models.py

--- download_models.py
+++ download_models.py
@@ -3,8 +3,6 @@ import os
 
 local_dir="pretrained_ckpts/"
 os.makedirs(local_dir, exist_ok=True)
-snapshot_download(repo_id="nvidia/Cosmos-1.0-Tokenizer-CV8x8x8", local_dir=local_dir+"Cosmos-1.0-Tokenizer-CV8x8x8")
-
 cache_dir = local_dir + "/cache"
 
 snapshot_download(cache_dir=cache_dir,
@@ -15,3 +13,11 @@ snapshot_download(cache_dir=cache_dir,
   allow_patterns=["*.json", "*.safetensors", "*.bin", "*.py", "*.md", "*.txt"],
 )
 
+
+snapshot_download(cache_dir=cache_dir,
+  local_dir=local_dir+"BAGEL-7B-MoT",
+  repo_id="Wan-AI/Wan2.1-I2V-14B-720P",
+  local_dir_use_symlinks=False,
+  resume_download=True,
+  allow_patterns=["*VAE.pth"],
+)
