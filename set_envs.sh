5721846015522c5143ce7915577438ebccc4e833
Juncheng Ma <101777868+ucasmjc@users.noreply.github.com>
2025-06-24 19:59:03 +0800

    Update set_envs.sh

--- set_envs.sh
+++ set_envs.sh
@@ -4,15 +4,9 @@ cd bagel_video
 conda env create -f ./env.yml
 conda activate bagel_train
 pip install flash_attn==2.5.8 --no-build-isolation
-#cosmos
-git clone https://github.com/NVIDIA/Cosmos-Tokenizer.git
-cd Cosmos-Tokenizer
-sudo apt-get install -y ffmpeg git-lfs
-git lfs pull
-pip3 install -e .
 
-#下载预训练权重
-cd ..
+sudo apt-get install -y ffmpeg git-lfs
+
 python download_models.py
 cp tokenizer_config.json pretrained_ckpts/BAGEL-7B-MoT
-#下载T2I adapt权重
+
